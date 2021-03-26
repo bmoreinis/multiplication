9b91d1bc9ce48b822a8808ac80329e2715b67243
Bram Moreinis <bmoreinis@gmail.com>
2021-03-26 16:25:55 +0000

    removed code

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 /* Initialize Global Variables */
-var x=0, y=0, q = 0, m = 0;
+var x=0, y=0, mistakes = 0;
 
 /* Main Control Structure */
 function main(){
-  for(q = 1;q < 6; q++){
-    x = Math.floor(Math.random()*9)+1;
-    y = Math.floor(Math.random()*9)+1;
-    m += ask(x,y,0);
-  }
-  alert("Total mistakes in "+(q-1)+" equations: "+m);
+ alert("Run Program!");
 }
 
 /* User Interaction Function */
-function ask(x,y,m){
-  let answer = prompt("What is "+x+" * "+y+"?");
-  if (answer != x*y) {
-    alert("Wrong! Try again.");
-    m++;
-    ask(x,y,m);
-  }
-  else alert("Correct after "+ m + " mistakes!");
-  return m;
+function ask(x,y,mistakes){
+  return mistakes;
 }
